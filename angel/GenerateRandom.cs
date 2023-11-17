f3aff0c2117ef93a4cc1b7f367f84eb4870e1b09
JPKakaAG <Gjopka@yandex.ru>
2023-11-17 16:21:17 +0300

    update

---
+++ angel/GenerateRandom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace angel
+{
+    internal class GenerateRandom
+    {
+        public static int[] GenerateRandomArray(int min, int max)
+        {
+            Random random = new Random();
+            int min2 = Math.Abs(min);
+            int max2 = Math.Abs(max);   
+            int count = min2 + max;
+            int[] array = new int[count];
+            for (int i = 0; i < count ; i++)
+            {
+                array[i] = min++;
+            }
+            return array;
+        }
+    }
+}
--- angel/MainWindow.xaml.cs
+++ angel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,39 @@ namespace angel
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<int> numbers;
         public MainWindow()
         {
             InitializeComponent();
+            numbers = new ObservableCollection<int>();
+
         }
+        private void btnCalculate_click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tb_ot.Text, out int A) && int.TryParse(tb_do.Text, out int B))
+            {
+                int evenCount = 0;
+                int oddCount = 0;
+                int[] array = (GenerateRandom.GenerateRandomArray(A, B));
+                for (int i = 0; i < array.Length; i++) 
+                {
+                    numbers.Add(array[i]);
+                }
+               
+                foreach (int number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        evenCount++; // Увеличиваем счетчик четных чисел
+                    }
+                    else
+                    {
+                        oddCount++; // Увеличиваем счетчик нечетных чисел
+                    }
+                }
+
+                lb_calc.Items.Add($"Количество четных чисел: {evenCount}\r\nКоличество нечетных чисел: {oddCount}");
+            }         
+        }          
     }
 }
